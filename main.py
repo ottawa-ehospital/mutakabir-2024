ee32bce4999acc4f8ecf985977a35b1ec18a69b9
Abdul Muatakabbir <47092538+AbdulMutakabbir@users.noreply.github.com>
2024-03-06 16:00:03 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,89 @@
-from fastapi import FastAPI
+# from fastapi import FastAPI
 
-from api import prediction 
+# from api import prediction 
+
+# app = FastAPI()
+
+# # Include API routers
+# # app.include_router(prediction.router, tags=["prediction"])
+
+# @app.get("/")
+# def read_root():
+#     return {"message": "Hello, FastAPI!"}
+
+# # if __name__ == "__main__":
+# #     import uvicorn
+
+# #     uvicorn.run(app)
+
+from fastapi import FastAPI, Path
+from typing import Optional
+from pydantic import BaseModel
+
 
 app = FastAPI()
 
-# Include API routers
-# app.include_router(prediction.router, tags=["prediction"])
+students = {
+    1: {
+        "name": "john",
+        "age": 17,
+        "year": "year 12"
+    }
+}
+
+class Student(BaseModel):
+    name: str
+    age: int
+    year: str
+
+class UpdateStudent(BaseModel):
+    name: Optional[str] = None
+    age: Optional[int] = None
+    year: Optional[str] = None
 
 @app.get("/")
-def read_root():
-    return {"message": "Hello, FastAPI!"}
+def index():
+    return {"name": "First Data"}
 
-# if __name__ == "__main__":
-#     import uvicorn
+@app.get("/get-student/{student_id}")
+def get_student(student_id: int = Path(None, description="The ID of the student you want to view", gt=0, lt=3)):
+    return students[student_id]
 
-#     uvicorn.run(app)
+@app.get("/get-by-name/{student_id}")
+def get_student(*, student_id: int, name: Optional[str] = None, test : int):
+    for student_id in students:
+        if students[student_id]["name"] == name:
+            return students[student_id]
+    return {"Data": "Not found"}
+
+@app.post("/create-student/{student_id}")
+def create_student(student_id : int, student : Student):
+    if student_id in students:
+        return {"Error": "Student exists"}
+
+    students[student_id] = student
+    return students[student_id]
+
+@app.put("/update-student/{student_id}")
+def update_student(student_id: int, student: UpdateStudent):
+    if student_id not in students:
+        return {"Error": "Student does not exist"}
+
+    if student.name != None:
+        students[student_id].name = student.name
+
+    if student.age != None:
+        students[student_id].age = student.age
+
+    if student.year != None:
+        students[student_id].year = student.year
+
+    return students[student_id]
+
+@app.delete("/delete-student/{student_id}")
+def delete_student(student_id: int):
+    if student_id not in students:
+        return {"Error": "Student does not exist"}
+
+    del students[student_id]
+    return {"Message": " Student deleted successfully"}
