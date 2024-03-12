d2faca3fc3262f4954c4c4ca992c0cc9e7e40a8b
Abdul Muatakabbir <47092538+AbdulMutakabbir@users.noreply.github.com>
2024-03-12 08:21:09 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://www.e-hospital.ca/",
-        "https://www.e-hospital.ca/",
+        "http://www.e-hospital.ca",
+        "https://www.e-hospital.ca",
         "http://localhost:3000",
         "https://localhost:3000",
     ],
